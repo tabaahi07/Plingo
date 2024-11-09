9fe31dfbf97badd257264a0c436409c30e9013ce
tabaahi07 <126075036+tabaahi07@users.noreply.github.com>
2024-11-09 13:30:13 +0530

    Project setup

---
+++ src/main/java/in/wimsyclimsy/plingo/PlingoApplication.java
@@ -0,0 +1,13 @@
+package in.wimsyclimsy.plingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/wimsyclimsy/plingo/PlingoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.wimsyclimsy.plingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
